7f21025a2691e63e1fd9bf6148501a7805ac0de3
hdseo <terranpushs@naver.com>
2021-03-28 20:21:04 +0900

    git init

---
+++ coin-flow-chart/src/main/java/com/crypto/CoinFlowChartApplication.java
@@ -0,0 +1,13 @@
+package com.crypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinFlowChartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinFlowChartApplication.class, args);
+	}
+
+}
