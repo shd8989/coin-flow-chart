dd61ebb77a13036eb16937890bdfd43caa785be4
hyundong-seo <terranpush@naver.com>
2024-01-26 12:50:53 +0900

    삭제 - 코드 정리

--- src/main/java/com/crypto/service/IndexService.java
+++ src/main/java/com/crypto/service/IndexService.java
@@ -2,14 +2,11 @@ package com.crypto.service;
 
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 import com.crypto.model.CandleEntity;
-import org.json.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
@@ -51,60 +48,6 @@ public class IndexService {
 			return responseEntityList;
 		}
 		return responseEntityList;
-	}
-	
-	public int detailCoinData(List<Map> coinReqList) {
-		try {
-			HttpHeaders httpHeaders = new HttpHeaders();
-			httpHeaders.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-			HttpEntity entity = new HttpEntity(httpHeaders);
-			
-	//		코인 상세정보
-			String coinDetailUrl = "https://api.upbit.com/v1/ticker?markets=";
-			RestTemplate coinDetailRestTemplate = new RestTemplate();
-			List<Map> coinDetailResList = new ArrayList<Map>();
-			for(int i=0; i<coinReqList.size(); i++) {
-				ResponseEntity<ArrayList> coinDetailListResponseEntity = coinDetailRestTemplate.exchange(coinDetailUrl+coinReqList.get(i).get("market"), HttpMethod.GET, entity, ArrayList.class);
-				List<CoinEntity> coinDetailResponseList = (List) coinDetailListResponseEntity.getBody();
-				Iterator itr1 = coinDetailResponseList.iterator();
-				
-				while(itr1.hasNext()) {
-					Map map = (Map)itr1.next();
-					Map newMap = new HashMap();
-					newMap.put("market", map.get("market"));
-					newMap.put("trade_date", map.get("trade_date"));
-					newMap.put("trade_time", map.get("trade_time"));
-					newMap.put("trade_date_kst", map.get("trade_date_kst"));
-					newMap.put("trade_time_kst", map.get("trade_time_kst"));
-					newMap.put("trade_timestamp", map.get("trade_timestamp"));
-					newMap.put("opening_price", map.get("opening_price"));
-					newMap.put("high_price", map.get("high_price"));
-					newMap.put("low_price", map.get("low_price"));
-					newMap.put("trade_price", map.get("trade_price"));
-					newMap.put("prev_closing_price", map.get("prev_closing_price"));
-					newMap.put("change", map.get("change"));
-					newMap.put("change_price", map.get("change_price"));
-					newMap.put("change_rate", map.get("change_rate"));
-					newMap.put("signed_change_price", map.get("signed_change_price"));
-					newMap.put("signed_change_rate", map.get("signed_change_rate"));
-					newMap.put("trade_volume", map.get("trade_volume"));
-					newMap.put("acc_trade_price", map.get("acc_trade_price"));
-					newMap.put("acc_trade_price_24h", map.get("acc_trade_price_24h"));
-					newMap.put("acc_trade_volume", map.get("acc_trade_volume"));
-					newMap.put("acc_trade_volume_24h", map.get("acc_trade_volume_24h"));
-					newMap.put("highest_52_week_price", map.get("highest_52_week_price"));
-					newMap.put("highest_52_week_date", map.get("highest_52_week_date"));
-					newMap.put("lowest_52_week_price", map.get("lowest_52_week_price"));
-					newMap.put("lowest_52_week_date", map.get("lowest_52_week_date"));
-					newMap.put("timestp", map.get("timestp"));
-					coinDetailResList.add(newMap);
-				}
-			}
-			return indexMapper.updateCoinAllData(coinDetailResList);
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}
-		return 0;
 	}
 
 	public List<CandleEntity> selectCandleData(Map reqMap) {
